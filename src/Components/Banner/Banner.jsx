3e38428803d1afa3566b14c7eeca7cf636cecdca
ShaminasP <mrzeddem@gmail.com>
2023-01-13 03:33:17 +0530

    API integrated

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { API_KEY, imageUrl } from "../../Constants/Constants";
+import axios from "../../axios";
 import './Banner.css'
+import { useState } from "react";
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+      console.log(response.data.results[0]);
+      setMovie(response.data.results[7]);
+    })
+  }
+    , [])
+
   return (
-    <div className="banner">
+    <div
+      style={{backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:""})`}}
+      className="banner">
       <div className="content">
-        <h1 className="title">Movie name</h1>
+        <h1 className="title">{movie ? movie.title : ""}</h1>
         <div className="banner_buttons">
           <button className="button">Play</button>
           <button className="button">My List</button>
         </div>
-        <h1 className="discription">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</h1>
+        <h1 className="discription">{movie ? movie.overview : ""}</h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
